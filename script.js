8dde8948fdf37d4a0c9a8fdc2dd5ed7cfc7c3d08
Bae5149 <ana.samanabaena@alu.frt.utn.edu.ar>
2026-04-04 05:05:16 -0300

    Se agrego segundo cuerpo de prueba

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Mapa de modelos: cada nombre de modelo apunta a su archivo GLB
 const modelos = {
-    'cubo': 'modelos/cuerpo1.glb'
+    'cubo': 'modelos/cuerpo1.glb',
+    'cubo perforado': 'modelos/cuerpo2.glb'
 };
 
 // Obtener el modelo desde la URL 
